3b7d1ba15169f5d8e75a45fc4204ce631098a6e9
Jevon Longdell <jevon.longdell@gmail.com>
2022-02-19 12:26:23 +1300

    Update countdown_word_game.py
    modified to use wordfreq package rather than /usr/share/dict/
    words

--- countdown_word_game.py
+++ countdown_word_game.py
@@ -1,12 +1,15 @@
 from multiset import Multiset
 
+import wordfreq
 
+words = wordfreq.iter_wordlist('en')
 #givenletters = Multiset(list('redaardvark'))
 
 givenletters = Multiset(list(input("Enter given letters: ").strip()))
 
-words = map(str.strip, open('/usr/share/dict/words').readlines() )
-words = map(str.lower, words)
+#words = map(str.strip, open('/usr/share/dict/words').readlines() )
+#words = map(str.lower, words)
+
 
 
 found_words_dict = {}
@@ -23,6 +26,7 @@ print(givenletters)
 print()
                 
 for k in sorted(found_words_dict):
+    found_words_dict[k].sort(reverse=True,key=(lambda w: wordfreq.word_frequency(w,'en')))
     print('%d letter words:'%(k,))
     print(found_words_dict[k])
     print()
